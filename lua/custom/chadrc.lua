a9913ef2f0c7ce70dbde81968bfaa3bbb68bee6c
ayushmishra <mail81mitthu@gmail.com>
2023-06-13 10:56:06 +0530

    refactored lsconfig

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -19,7 +19,7 @@ M.ui = {
     theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
-    separator_style = "",
+    separator_style = "arrow",
   },
 
   telescope = {
--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,28 +1,22 @@
--- custom.plugins.lspconfig
--- local on_attach = require("plugins.configs.lspconfig").on_attach
--- local capabilities = require("plugins.configs.lspconfig").capabilities
---
--- local lspconfig = require "lspconfig"
--- local servers = { "rust_analyzer", "pyright" }
---
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = on_attach,
---     capabilities = capabilities,
---   }
--- end
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
-lspconfig.pyright.setup { on_attach = on_attach, capabilities = capabilities }
+local servers = { "pyright" }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+-- Configured rust_analyzer different ,needs to have some more settings
 lspconfig.rust_analyzer.setup {
   on_attach = on_attach,
-  settings = { 
+  settings = {
     ["rust-analyzer"] = {
-      cargo = { 
-        extraArgs = { "-r" } 
-      } 
-    } 
+      cargo = {
+        extraArgs = { "-r" },
+      },
+    },
   },
 }
