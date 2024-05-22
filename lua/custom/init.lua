4c170b3d9c61505c12d0dd1f25f76157055b6951
Ayush Mishra <mail81mitthu@gmail.com>
2024-05-23 01:11:04 +0530

    toogle autocomplete
    jsut as mr zozing said

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,3 +6,20 @@ opt.numberwidth = 1
 opt.ruler = true
 
 opt.timeoutlen = 200
+
+local function toggle_autocomplete()
+  local cmp = require('cmp')
+  local current_setting = cmp.get_config().completion.autocomplete
+  if current_setting and #current_setting > 0 then
+    cmp.setup({ completion = { autocomplete = false } })
+    print('Autocomplete disabled')
+  else
+    cmp.setup({ completion = { autocomplete = { cmp.TriggerEvent.TextChanged } } })
+    print('Autocomplete enabled')
+  end
+end
+
+vim.api.nvim_create_user_command('C', toggle_autocomplete, {})
+
+-- Set a keymap like this for example:
+-- vim.api.nvim_set_keymap('n', '<Leader>a', ':NvimCmpToggle<CR>', { noremap = true, silent = true })
