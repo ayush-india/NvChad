8331abe71c1cdea984bb27f314fe14267e020e24
ayush mishra <mail81mitthu@gmail.com>
2024-10-21 02:28:11 +0530

    removed shitty colorschemce ( catppuccin )

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -98,21 +98,6 @@ local plugins = {
   "ThePrimeagen/harpoon",
 
   "ThePrimeagen/vim-be-good",
-
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup {
-        transparent_background = true,
-      }
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
-
-  -- Lua
   {
     "folke/zen-mode.nvim",
     lazy = false,
